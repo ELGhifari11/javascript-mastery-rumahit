94c04e1c05b02144d6e60670ab26fe1a6f4b77fc
ELGhifari11 <ghifariakun@gmail.com>
2025-11-06 11:01:29 +0700

    feat: add new function for example push() methode for array manipulation

--- batch23/pertemuan7/script.js
+++ batch23/pertemuan7/script.js
@@ -1,4 +1,10 @@
 
+
+// Default Notification
+function togglePopup(no) {
+    document.getElementById(`popupModal${no}`).classList.toggle("hidden");
+}
+
 // Closure - Switch - IF with Operator - Hoisting - Comparasion & Logical - Looping - 
 
 
@@ -25,5 +31,36 @@ function f2(namaData) {
         }
     }
 }
+// f2("Data Santri")(arr3)
 
-f2("Data Santri")(arr3)
+
+// ================================================
+// FUNCTION ADD DATA TO ARRAY
+// ================================================
+
+const dataSantri = ['Bambang']
+const output = document.getElementById('outputBox');
+
+document.getElementById("F1").addEventListener("submit", function (e) {
+    e.preventDefault();
+    togglePopup("1")
+    const name = document.getElementById("name").value;
+    if (!name) {
+        alert("Data Belum Ke Input");
+    } else {
+        manipulationElementArray(name)
+    }
+});
+
+function manipulationElementArray(data) {
+    togglePopup('1')
+    dataSantri.push(data), // PUSH()
+    output.innerText = dataSantri
+    console.log(`Data ${data} berhasil Di tambahkan ...`);
+    togglePopup('1')
+}
+
+
+
+
+
