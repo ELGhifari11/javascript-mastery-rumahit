ba01dca91a32ff3c0ed612f1ca471f6431766e6c
ELGhifari11 <ghifariakun@gmail.com>
2025-11-07 11:19:26 +0700

    feat: example excution for find() method

--- batch23/pertemuan8/script.js
+++ batch23/pertemuan8/script.js
@@ -7,7 +7,11 @@ let hasilSplice = arr.splice(1,1,'JIJI',"909090","9009","data Baru")
 //  console.log(hasilSplice); // LENGHT MASIH 
 
 // ==================================================
-// MANIPULATION ARRAY WITH ONLY METHOD FOR ARRAY 
+// ==================================================
+
+//   MANIPULATION ARRAY WITH ONLY METHOD FOR ARRAY 
+
+// ==================================================
 // ==================================================
 
 const namaIslami = [
@@ -68,7 +72,10 @@ const namaIslami = [
 //     // console.log(a);
 // })
 
-let hasilEdit = []
+// ==================================================
+// MANIPULATION ARRAY WITH FOREACH() 
+// ==================================================
+
 
 // namaIslami.forEach((a,b,c) => { // PARAM (1. Value , 2. Index, 3. Array) 
 //     // console.log(a);
@@ -81,6 +88,18 @@ let hasilEdit = []
 
 // console.table(namaIslami);
 
+// ==================================================
+// MANIPULATION ARRAY WITH MAP() 
+// ==================================================
+
+let angka = [1,2,3,4,5]
+
+let resultMap = angka.map((v,i,a)=>{
+    return `Hasil ${v} x 2 = ${v*2}`;
+})
+
+// console.table(angka);
+// console.table(resultMap);
 
 let hasilMap = namaIslami.map((v,i,a)=>{
    if(i % 2 == 0) {
@@ -90,6 +109,9 @@ let hasilMap = namaIslami.map((v,i,a)=>{
    } 
 })
 
+// ==================================================
+// MANIPULATION ARRAY WITH FILTER() 
+// ==================================================
 
 let rit = hasilMap.filter((v)=>{
     return v.includes("RIT");
@@ -99,20 +121,43 @@ let pit = hasilMap.filter((v)=>{
     return v.includes("PIT");
 })
 
+// console.group("TABLE DATA ARRAY AWAL");
+// console.table(namaIslami);
+// console.groupEnd();
 
-console.group("TABLE DATA ARRAY AWAL");
-console.table(namaIslami);
-console.groupEnd();
+// console.group("TABLE DATA ARRAY HASIL MODIFIKASI MAP() ");
+// console.table(hasilMap);
+// console.groupEnd();
 
-console.group("TABLE DATA ARRAY HASIL MODIFIKASI MAP() ");
-console.table(hasilMap);
-console.groupEnd();
-console.group("TABLE DATA ARRAY HASIL MODIFIKASI FILTER() RIT ");
-console.table(rit);
-console.groupEnd();
+// console.group("TABLE DATA ARRAY HASIL MODIFIKASI FILTER() RIT ");
+// console.table(rit);
+// console.groupEnd();
+
 console.group("TABLE DATA ARRAY HASIL MODIFIKASI FILTER() PIT ");
 console.table(pit);
 console.groupEnd();
+
+
+ // ==================================================
+// MANIPULATION ARRAY WITH FIND() 
+// ==================================================
+
+function searcName(name) {
+
+    let resultFind = pit.find((v,i,a)=>{
+        return v.includes(name)
+    })
+
+    pit.forEach((v,i,a)=>{
+        if(resultFind == v) {
+            console.log(`Nama ${v} Ada Pada Index ke ${i}`);
+        }
+    })
+}
+
+// SOLVE BAGIAN INI MENJADI FUNGSI YANG BERJALAN
+searcName('amir')
+
 
 
 
