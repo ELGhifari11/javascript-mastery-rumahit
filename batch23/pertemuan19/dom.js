dd5a0127e77198125b5a36abaf3c67b5633568e9
ELGhifari11 <ghifariakun@gmail.com>
2025-11-22 08:09:15 +0700

    feat: add DOM manipulation script for element styling and content

---
+++ batch23/pertemuan19/dom.js
@@ -0,0 +1,11 @@
+const tagDiv1 = document.getElementById("id1")
+const tagH1 = document.getElementById("id1")
+
+tagH1.style.color = 'red'
+
+tagDiv1.innerHTML = "<p>INI DARI JS 1</p>"
+
+tagDiv1.innerText = "INI INNER TEXT JS"
+
+
+
