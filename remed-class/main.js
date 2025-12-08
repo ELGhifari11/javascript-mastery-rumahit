76085f0471cc4471a60599a443f2f3fd8254f079
ELGhifari11 <ghifariakun@gmail.com>
2025-12-08 11:18:38 +0700

    feat: add main.js with utility and db function calls

---
+++ remed-class/main.js
@@ -0,0 +1,64 @@
+import * as utils from "./utils/index.js"
+import * as db from "./db/index.js"
+
+console.log(utils.f1());
+console.log(db.f1());
+
+console.log(utils.f2("Mie Ayam","Teh","Pangsit","Bakso"));
+
+
+
+// =========================================
+
+// console.log("Hello World");
+
+// function f1() {
+//     for (let index = 0; index == 5 ; index++) {
+//          var a = 10 
+//         // console.log(a);
+//     } 
+//     return  a
+// }
+
+// function f3() {
+//     return "INI F3"
+// }
+
+// const f2 = () => {
+//     return "INI F2"
+// }
+
+// const sapa = (nama) => `Halo ${nama}.`
+// const sapa2 = (nama) => "Halo" + nama + "."
+
+// console.log(f2());
+
+
+// function hitungLuas(p, l) {
+//   return p * l;
+// }
+
+
+// const hitungLuas2 = (p, l) => p * l;
+
+// console.log(hitungLuas2(100,10));
+
+
+// // Gunakan arrow function untuk menghasilkan array baru berisi angka kali 2.
+// const angka = [1, 2, 3, 4, 5];
+
+// function kali(arr) {
+//    let hasil =  arr.map((v)=> {
+//         return v * 2
+//     })
+//     return hasil
+// }
+
+// const kaliDua = angka.map(v => v*2)
+
+// // console.log(kaliDua);
+
+// console.log(kali(angka));
+
+
+
