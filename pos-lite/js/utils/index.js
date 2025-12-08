d811ab1f5d28c89020c31a14c909c17ae2ea670a
ELGhifari11 <ghifariakun@gmail.com>
2025-12-08 08:12:56 +0700

    feat: Introduce utility modules for navigation, UI components, and general helpers.

---
+++ pos-lite/js/utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * ==================================================================================
+ * UTILS MODULE (INDEX)
+ * ==================================================================================
+ * Menggabungkan semua helper function dari sub-module.
+ */
+
+export * from './dom.js';
+export * from './format.js';
+export * from './ui.js';
+export * from './nav.js';
+export * from './others.js';
