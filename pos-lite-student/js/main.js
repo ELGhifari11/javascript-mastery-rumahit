60eecb0fada14e04056eed225898ad55e2d8ed2b
ELGhifari11 <ghifariakun@gmail.com>
2025-12-12 20:59:54 +0700

    fix: clean up console.table usage and simplify render function in main.js

--- pos-lite-student/js/main.js
+++ pos-lite-student/js/main.js
@@ -5,7 +5,7 @@ import * as db from './db/index.js';
 
 console.log("Mesin POS Lite dinyalakan..."); // Cek di Console Browser (F12)
 
-console.table(db.myProduct); // [{}]
+console.table(db.myProduct);
 
 
 // 2. Ambil wadah dari index.html
@@ -15,15 +15,6 @@ const wadahAplikasi = document.getElementById('aplikasi');
 const styleContainer = 'display: flex; align-items: start; flex-wrap: wrap; gap: 20px; padding:10px';
 wadahAplikasi.setAttribute('style',styleContainer)
 
-// 3. Data Bohongan (Dummy) untuk tes
-const produkTest = {
-    nama: "Kopi Susu Gula Aren",
-    harga: 18000,
-    kategori: "Minuman",
-};
-
-// 4. Bikin Tampilan pakai fungsi sakti 'buatElemen'
-// Perhatikan betapa bersihnya kode ini dibanding document.createElement biasa!
-const renderBanyakKartu = (db) => db.forEach(v => wadahAplikasi.appendChild(utils.renderSingleKartu(v)))
+const renderBanyakKartu = (products) => products.forEach(v => wadahAplikasi.appendChild(utils.renderSingleKartu(v)))
 
 wadahAplikasi.appendChild(renderBanyakKartu(db.myProduct))
