f3eb3d4a005b1648dd09d0e33f7372f8e5b2a926
ELGhifari11 <ghifariakun@gmail.com>
2025-12-12 20:54:13 +0700

    feat: add export for ui.js in index.js

--- pos-lite-student/js/utils/index.js
+++ pos-lite-student/js/utils/index.js
@@ -4,4 +4,6 @@ export * from './dom.js';
 // Export semua (*) yang ada di file format.js
 export * from './format.js';
 
+export * from './ui.js'
+
 // Nanti kalau ada file baru, tinggal tambah di sini.
