46f092b19ecb7af4da3b89f742196331379380b8
ELGhifari11 <ghifariakun@gmail.com>
2025-12-25 22:26:33 +0700

    feat: Add product management functions for retrieving, adding, finding, updating, and deleting products.

--- pos-lite-student/js/db/products.js
+++ pos-lite-student/js/db/products.js
@@ -1,3 +1,7 @@
+
+import { ambilStatePOS, simpanStatePOS } from './core.js';
+
+
 export const myProduct = [
     {
         nama: "Kopi Susu Gula Aren",
@@ -73,3 +77,77 @@ export const myProduct = [
     },
     // ---------------------------
 ];
+
+
+
+/**
+ * Mengambil hanya daftar produk.
+ * @returns {Array} Array of Products
+ */
+export function ambilSemuaProduk() {
+    const state = ambilStatePOS();
+    // Safety check: kalau state.produk undefined, kembalikan array kosong
+    return state.produk || []; 
+}
+
+/**
+ * Menambah produk baru ke database.
+ * @param {Object} produkBaru - Object produk {id, nama, harga...}
+ */
+export function tambahProduk(produkBaru) {
+    // 1. Ambil State Terkini
+    const state = ambilStatePOS();
+    
+    // 2. Pastikan array produk ada
+    if (!state.produk) state.produk = [];
+    
+    // 3. Masukkan ke paling depan (unshift) biar muncul di atas
+    state.produk.unshift(produkBaru);
+    
+    // 4. Simpan kembali State yang sudah diupdate
+    simpanStatePOS(state);
+}
+
+/**
+ * Mencari produk spesifik berdasarkan ID.
+ * @param {String|Number} id 
+ */
+export function cariProdukById(id) {
+    const semua = ambilSemuaProduk();
+    // find akan mengembalikan Object produk atau undefined
+    return semua.find(p => p.id == id);
+}
+
+/**
+ * Mengupdate data produk.
+ * @param {String} id - ID produk yg mau diedit
+ * @param {Object} dataUpdate - Data baru (bisa parsial)
+ */
+export function perbaruiProdukById(id, dataUpdate) {
+    const state = ambilStatePOS();
+    
+    // Cari indexnya di array
+    const index = state.produk.findIndex(p => p.id == id);
+    
+    if (index !== -1) {
+        // Replace data lama dengan gabungan data baru
+        // ...state.produk[index] = data lama
+        // ...dataUpdate = data baru (menimpa yang lama)
+        state.produk[index] = {
+            ...state.produk[index],
+            ...dataUpdate
+        };
+        simpanStatePOS(state);
+    }
+}
+
+/**
+ * Menghapus produk.
+ * @param {String} id 
+ */
+export function hapusProdukById(id) {
+    const state = ambilStatePOS();
+    // Filter: Ambil semua produk yang BUKAN id ini
+    state.produk = state.produk.filter(p => p.id != id);
+    simpanStatePOS(state);
+}
